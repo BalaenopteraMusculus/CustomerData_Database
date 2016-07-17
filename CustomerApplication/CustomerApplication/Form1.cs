bdb4c7511a2757a57340cb541414e40d9fe2920f
RKStudios <r_kieu@hotmail.com>
2016-07-16 23:14:17 -0700

    Form dimensions

--- CustomerApplication/CustomerApplication/Form1.cs
+++ CustomerApplication/CustomerApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace CustomerApplication
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
