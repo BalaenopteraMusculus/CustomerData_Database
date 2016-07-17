fc1600b7e6b7199df32423807230984df4355fac
RKStudios <r_kieu@hotmail.com>
2016-07-16 17:31:51 -0700

    SQL Server Customer
    changed table name and added comment

--- SQLQuery_CustomerData.sql
+++ SQLQuery_CustomerData.sql
@@ -1,5 +1,6 @@
-Create table CustomerData
+Create table Customer
 (
+	--increments CustomerID
 	CustomerID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	CustomerName varchar(30) NOT NULL,
 	PhoneNumber varchar(30) NOT NULL,
