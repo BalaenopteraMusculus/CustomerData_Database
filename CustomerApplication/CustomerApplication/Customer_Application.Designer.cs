b3b6e13efa7ee05b24de9b85d539fafab2386d2d
RKStudios <r_kieu@hotmail.com>
2016-07-18 04:19:20 -0700

    C# form changes
    +implemented search, delete, add, liveupdate
    +update function still bugged

--- CustomerApplication/CustomerApplication/Customer_Application.Designer.cs
+++ CustomerApplication/CustomerApplication/Customer_Application.Designer.cs
@@ -29,22 +29,28 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.addButton = new System.Windows.Forms.Button();
-            this.deleteButton = new System.Windows.Forms.Button();
-            this.updateButton = new System.Windows.Forms.Button();
-            this.searchButton = new System.Windows.Forms.Button();
-            this.IDText = new System.Windows.Forms.TextBox();
-            this.NameText = new System.Windows.Forms.TextBox();
-            this.NumberText = new System.Windows.Forms.TextBox();
-            this.EmailText = new System.Windows.Forms.TextBox();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.NumberText = new System.Windows.Forms.TextBox();
+            this.EmailText = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.IDText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.oNameText = new System.Windows.Forms.TextBox();
+            this.oNumberText = new System.Windows.Forms.TextBox();
+            this.oEmailText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,76 +65,8 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(933, 515);
+            this.dataGridView1.Size = new System.Drawing.Size(933, 476);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // addButton
-            // 
-            this.addButton.Location = new System.Drawing.Point(366, 598);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(164, 40);
-            this.addButton.TabIndex = 2;
-            this.addButton.Text = "add";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // deleteButton
-            // 
-            this.deleteButton.Location = new System.Drawing.Point(671, 598);
-            this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(98, 40);
-            this.deleteButton.TabIndex = 3;
-            this.deleteButton.Text = "delete";
-            this.deleteButton.UseVisualStyleBackColor = true;
-            // 
-            // updateButton
-            // 
-            this.updateButton.Location = new System.Drawing.Point(26, 598);
-            this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(164, 40);
-            this.updateButton.TabIndex = 4;
-            this.updateButton.Text = "update";
-            this.updateButton.UseVisualStyleBackColor = true;
-            this.updateButton.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // searchButton
-            // 
-            this.searchButton.Location = new System.Drawing.Point(196, 598);
-            this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(164, 40);
-            this.searchButton.TabIndex = 5;
-            this.searchButton.Text = "search";
-            this.searchButton.UseVisualStyleBackColor = true;
-            this.searchButton.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // IDText
-            // 
-            this.IDText.Location = new System.Drawing.Point(26, 547);
-            this.IDText.Name = "IDText";
-            this.IDText.Size = new System.Drawing.Size(164, 20);
-            this.IDText.TabIndex = 6;
-            // 
-            // NameText
-            // 
-            this.NameText.Location = new System.Drawing.Point(196, 547);
-            this.NameText.Name = "NameText";
-            this.NameText.Size = new System.Drawing.Size(298, 20);
-            this.NameText.TabIndex = 7;
-            // 
-            // NumberText
-            // 
-            this.NumberText.Location = new System.Drawing.Point(500, 547);
-            this.NumberText.Name = "NumberText";
-            this.NumberText.Size = new System.Drawing.Size(165, 20);
-            this.NumberText.TabIndex = 8;
-            // 
-            // EmailText
-            // 
-            this.EmailText.Location = new System.Drawing.Point(671, 547);
-            this.EmailText.Name = "EmailText";
-            this.EmailText.Size = new System.Drawing.Size(252, 20);
-            this.EmailText.TabIndex = 9;
             // 
             // Column1
             // 
@@ -158,56 +96,178 @@
             this.Column4.Name = "Column4";
             this.Column4.Width = 285;
             // 
-            // label1
+            // addButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 531);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
-            this.label1.TabIndex = 10;
-            this.label1.Text = " CustomerID";
+            this.addButton.Location = new System.Drawing.Point(440, 598);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(208, 40);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(713, 598);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(208, 40);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(226, 598);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(208, 40);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(12, 598);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(208, 40);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(177, 511);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(295, 20);
+            this.NameText.TabIndex = 7;
+            // 
+            // NumberText
+            // 
+            this.NumberText.Location = new System.Drawing.Point(478, 511);
+            this.NumberText.Name = "NumberText";
+            this.NumberText.Size = new System.Drawing.Size(180, 20);
+            this.NumberText.TabIndex = 8;
+            // 
+            // EmailText
+            // 
+            this.EmailText.Location = new System.Drawing.Point(667, 511);
+            this.EmailText.Name = "EmailText";
+            this.EmailText.Size = new System.Drawing.Size(254, 20);
+            this.EmailText.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(193, 531);
+            this.label2.Location = new System.Drawing.Point(174, 495);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 11;
-            this.label2.Text = "CustomerName";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Text = "NewCustomerName";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(497, 531);
+            this.label3.Location = new System.Drawing.Point(475, 495);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.Size = new System.Drawing.Size(97, 13);
             this.label3.TabIndex = 12;
-            this.label3.Text = "PhoneNumber";
+            this.label3.Text = "NewPhoneNumber";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(668, 531);
+            this.label4.Location = new System.Drawing.Point(664, 495);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 13;
-            this.label4.Text = "Email";
+            this.label4.Text = "NewEmail";
+            // 
+            // IDText
+            // 
+            this.IDText.Location = new System.Drawing.Point(12, 536);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(162, 20);
+            this.IDText.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 520);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "CustomerID";
+            // 
+            // oNameText
+            // 
+            this.oNameText.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.oNameText.Location = new System.Drawing.Point(180, 559);
+            this.oNameText.Name = "oNameText";
+            this.oNameText.Size = new System.Drawing.Size(292, 20);
+            this.oNameText.TabIndex = 16;
+            // 
+            // oNumberText
+            // 
+            this.oNumberText.Location = new System.Drawing.Point(478, 559);
+            this.oNumberText.Name = "oNumberText";
+            this.oNumberText.Size = new System.Drawing.Size(180, 20);
+            this.oNumberText.TabIndex = 17;
+            // 
+            // oEmailText
+            // 
+            this.oEmailText.Location = new System.Drawing.Point(667, 559);
+            this.oEmailText.Name = "oEmailText";
+            this.oEmailText.Size = new System.Drawing.Size(254, 20);
+            this.oEmailText.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 543);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "OldCustomerName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(475, 543);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "OldPhoneNumber";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(664, 543);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "OldEmail";
             // 
             // Customer_Application
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 650);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.oEmailText);
+            this.Controls.Add(this.oNumberText);
+            this.Controls.Add(this.oNameText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IDText);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.EmailText);
             this.Controls.Add(this.NumberText);
             this.Controls.Add(this.NameText);
-            this.Controls.Add(this.IDText);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.deleteButton);
@@ -234,14 +294,20 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.TextBox IDText;
         private System.Windows.Forms.TextBox NameText;
         private System.Windows.Forms.TextBox NumberText;
         private System.Windows.Forms.TextBox EmailText;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox IDText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox oNameText;
+        private System.Windows.Forms.TextBox oNumberText;
+        private System.Windows.Forms.TextBox oEmailText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- CustomerApplication/CustomerApplication/Customer_Application.cs
+++ CustomerApplication/CustomerApplication/Customer_Application.cs
@@ -14,11 +14,7 @@ namespace CustomerApplication
     public partial class Customer_Application : Form
     {
         SqlDataAdapter sda;
-        SqlCommandBuilder scb;
-        DataTable dt;
 
-        //create sql command string variable
-        SqlCommand command = new SqlCommand();
 
         //create connection object SqlConnection
         //specify connection string for object 
@@ -28,9 +24,8 @@ namespace CustomerApplication
         public Customer_Application()
         {
             InitializeComponent();
- 
-            //sConnect.ConnectionString = "data source = RK-PC\\SQLEXPRESS; database = CustomerData; integrated security = SSPI";
 
+            DataTable dt;
             dt = new DataTable();
 
             //SQL command to retrieve all data using sql connection
@@ -44,49 +39,120 @@ namespace CustomerApplication
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //sets sql string command connection object
-            command.Connection = sConnect;
+            live_update();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            scb = new SqlCommandBuilder(sda);
-            sda.Update(dt);
-        }
-
+        //search button
         private void button4_Click(object sender, EventArgs e)
         {
 
+            sConnect.Open();
+            SqlCommand command = sConnect.CreateCommand();
+
+            command.CommandType = CommandType.Text;
+
+            //select table entries where old CustomerID, CustomerName, PhoneNumber, Email are equal to 'old' text boxes
+            command.CommandText = "select * from Customer where (CustomerID ='" + IDText.Text + "') OR (CustomerName ='" + oNameText.Text + "') OR (PhoneNumber = '" + oNumberText.Text + "') OR (Email = '" + oEmailText.Text + "')";
+
+            command.ExecuteNonQuery();
+
+            //fill the data table with selected entries
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            sConnect.Close();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        //update button
+        //SQL command error
+        private void button3_Click(object sender, EventArgs e)
         {
+            /*
+            sConnect.Open();
 
+            //create sql command string variable
+            SqlCommand command = sConnect.CreateCommand();
+
+            command.CommandType = CommandType.Text;
+
+            //update table entries by replacing entries in 'old' text box entries with 'new' text box entries
+            command.CommandText = "update Customer set ((CustomerName ='" + NameText.Text + "') where (CustomerName = ''" + oNameText.Text + "')) OR ((PhoneNumber = '" + NumberText.Text + "') where (PhoneNumber = '" + oNumberText.Text + "')) OR ((Email = '" + EmailText.Text + "' )  where (Email = '" + oEmailText.Text + "'))";
+
+            command.ExecuteNonQuery();
+            sConnect.Close();
+            live_update();
+            MessageBox.Show("Customer data updated.");
+            */
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        //add button
+        private void button1_Click(object sender, EventArgs e)
         {
+            //open database connection
+            sConnect.Open();
+
+            //create sql command string variable
+            SqlCommand command = sConnect.CreateCommand();
+
+            command.CommandType = CommandType.Text;
+
+            //insert 'new' Customer data entries
+            command.CommandText = "insert into Customer values('"+NameText.Text+"','"+NumberText.Text+"','"+EmailText.Text+"')";
+
+            command.ExecuteNonQuery();
+            sConnect.Close();
+            live_update();
+            MessageBox.Show("Customer data added.");
+        }
+
+        //delete button
+        //deletes by CustomerID
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //open database connection
+            sConnect.Open();
+
+            //create sql command string variable
+            SqlCommand command = sConnect.CreateCommand();
+
+            command.CommandType = CommandType.Text;
+
+            //delete Customer data by CustomerID 
+            command.CommandText = "delete from Customer where CustomerID = '" + IDText.Text + "'";
+
+            command.ExecuteNonQuery();
+            sConnect.Close();
+            live_update();
+
+            //attempt to reseed primary key
+            command.CommandText = "DBCC CHECKIDENT (Customer, RESEED, 0)";
+            live_update();
+
+            MessageBox.Show("Customer data deleted.");
+        }
+
+        //live updates Customer table
+        public void live_update()
+        {
+            sConnect.Open();
+            SqlCommand command = sConnect.CreateCommand();
+            command.CommandType = CommandType.Text;
+
+            //selects current data from Customer db
+            command.CommandText = "select * from Customer";
+            command.ExecuteNonQuery();
+
+            //fills current data in new table
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            sConnect.Close();
 
         }
+
+
     }
 }
